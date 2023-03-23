a8e8981d6fc48881d4ff8af1ee4f5900ff893a0d
JulianoF <juliano1@live.ca>
2023-03-23 12:43:27 -0400

    Start Commit

---
+++ src/main/java/Main/DataStructuresFINAL.java
@@ -0,0 +1,16 @@
+package Main;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author juliano
+ */
+public class DataStructuresFINAL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
